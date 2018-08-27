f75a2af6e96d4f140ecc99934d74703cd82ec2ee
Rishabh <rishabh1997@gmail.com>
2018-08-27 10:47:17 +0530

    updated code

--- server/server.js
+++ server/server.js
@@ -68,6 +68,26 @@ res.send({todo});
 });
 });
 
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send(todo);
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+
+
 app.listen(port, () => {
   console.log(`connected to port${port}`);
 });
