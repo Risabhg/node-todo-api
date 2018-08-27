fbbdc9dbe0d4c840aa16700406d390fbf95e4c68
Rishabh <rishabh1997@gmail.com>
2018-08-27 11:07:59 +0530

    updated code

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -101,3 +101,26 @@ it('should return 404 if object not found',(done)=>{
   .end(done);
 });
 });
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    var hexId = todos[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.findById(hexId).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+});
